79fbc4f29b144616876fa4d2e3c23eba4c3cc236
Ziggy434 <68012775+Ziggy434@users.noreply.github.com>
2021-07-29 21:31:55 +0800

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,11 @@
+fx_version 'cerulean'
+games { 'rdr3', 'gta5' }
+
+author 'Ziggy'
+description 'Traffic reducer (all values can be changed in cl.lua to your liking :)'
+version '1.0.0'
+
+-- What to run
+client_scripts {
+    'cl.lua'
+}
